c003f9bca688be10a3e7526cf1462749a0298dc0
Arseniy Sazonov <rimus00@yandex.ru>
2022-09-26 23:30:32 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,136 @@
+const form = document.querySelector('.form');
+const textArea = document.querySelector('.text-area');
+const utro = document.querySelector('#utro');
+const den = document.querySelector('#den');
+const vech = document.querySelector('#vech');
+const obj = document.querySelector('#obj');
+const vse = document.querySelector('#vse');
+const platK = document.querySelector('#platK');
+const yaChe = document.querySelector('#yaChe');
+const delChe = document.querySelector('#delChe');
+const kidChe = document.querySelector('#kidChe');
+const svoyaChe = document.querySelector('#svoyaChe');
+const yanV = document.querySelector('#yanV');
+const delV = document.querySelector('#delV');
+const kidsV = document.querySelector('#kidsV');
+const svoyaV = document.querySelector('#svoyaV');
+const alcoG = document.querySelector('#alco');
+const dayS = document.querySelector('#dayS');
+const weekS = document.querySelector('#weekS');
+const monthS = document.querySelector('#monthS');
+const bkS = document.querySelector('#bkS');
+const manGS = document.querySelector('#manGS');
+
+function restoran () {
+  const cheki = vse.value - platK.value - yaChe.value - delChe.value - kidChe.value - svoyaChe.value;
+  const vsego = obj.value - yanV.value - delV.value - kidsV.value - svoyaV.value;
+  const procent = vsego / obj.value;
+  const text = `${cheki} / ${new Intl.NumberFormat("ru", {style: "currency", currency: "RUB", minimumFractionDigits: 0}).format(vsego)} / ${new Intl.NumberFormat("ru", {style: "percent", minimumFractionDigits: 1}).format(procent)}`;
+  return text;
+}
+
+function keytering () {
+  let text = '';
+  if (kidChe.value == 0) {
+    text += 0;
+  } else {
+    text += '\n';
+    text += '*Кидбург:*';
+    const procent = kidsV.value / obj.value;
+    text += `${kidChe.value} / ${new Intl.NumberFormat("ru", {style: "currency", currency: "RUB", minimumFractionDigits: 0}).format(kidsV.value)} / ${new Intl.NumberFormat("ru", {style: "percent", minimumFractionDigits: 1}).format(procent)}`;
+  }
+  return text;
+}
+
+function agregators () {
+  let text = '';
+  let procent = 0;
+  if (yaChe.value == 0 && delChe.value == 0 && svoyaChe.value == 0) {
+    console.log('asd')
+    text += 0;
+  } else {
+    if (yaChe.value > 0) {
+      text += '\n';
+      text += '*Яндекс:*';
+      procent = yanV.value / obj.value;
+      text += ` ${yaChe.value} / ${new Intl.NumberFormat("ru", {style: "currency", currency: "RUB", minimumFractionDigits: 0}).format(yanV.value)} / ${new Intl.NumberFormat("ru", {style: "percent", minimumFractionDigits: 1}).format(procent)}`;
+    }
+
+    if (delChe.value > 0) {
+      text += '\n';
+      text += '*Delivery:*';
+      procent = delV.value / obj.value;
+      text += ` ${delChe.value} / ${new Intl.NumberFormat("ru", {style: "currency", currency: "RUB", minimumFractionDigits: 0}).format(delV.value)} / ${new Intl.NumberFormat("ru", {style: "percent", minimumFractionDigits: 1}).format(procent)}`;
+    }
+
+    if (svoyaChe.value > 0) {
+      text += '\n';
+      text += '*Своя:*';
+      procent = svoyaV.value / obj.value;
+      text += ` ${svoyaChe.value} / ${new Intl.NumberFormat("ru", {style: "currency", currency: "RUB", minimumFractionDigits: 0}).format(svoyaV.value)} / ${new Intl.NumberFormat("ru", {style: "percent", minimumFractionDigits: 1}).format(procent)}`;
+    }
+  }
+  return text;
+}
+
+function alkogol () {
+  const procent = alcoG.value / obj.value;
+  const text = `${new Intl.NumberFormat("ru", {style: "currency", currency: "RUB", minimumFractionDigits: 0}).format(alcoG.value)} / ${new Intl.NumberFormat("ru", {style: "percent", minimumFractionDigits: 1}).format(procent)}`
+  return text;
+}
+
+function manG () {
+  if (manGS.value === 'lena'){
+    return 'Стоянова Елена';
+  }
+  if (manGS.value === 'vlad'){
+    return 'Бабкин Владислав';
+  }
+  if (manGS.value === 'arseniy'){
+    return 'Сазонов Арсений';
+  }
+}
+
+function sms () {
+  const inRes = restoran();
+  const keyT= keytering();
+  const agrE = agregators();
+  const alko = alkogol();
+  const manager = manG();
+  textArea.textContent += '*Добрый вечер!*\n';
+  textArea.textContent += '\n';
+  textArea.textContent += `*Отчёт по выручке ОМ ЦДМ за 25.09.2022*\n`;
+  textArea.textContent += '\n';
+
+  textArea.textContent += `*Утро:* ${new Intl.NumberFormat("ru", {style: "currency", currency: "RUB", minimumFractionDigits: 0}).format(utro.value)}\n`;
+  textArea.textContent += `*День:* ${new Intl.NumberFormat("ru", {style: "currency", currency: "RUB", minimumFractionDigits: 0}).format(den.value)}\n`;
+  textArea.textContent += `*Вечер:* ${new Intl.NumberFormat("ru", {style: "currency", currency: "RUB", minimumFractionDigits: 0}).format(vech.value)}\n`;
+  textArea.textContent += `*Общая:* ${new Intl.NumberFormat("ru", {style: "currency", currency: "RUB", minimumFractionDigits: 0}).format(obj.value)}\n`;
+  textArea.textContent += '\n';
+
+  textArea.textContent += '*Из них:*\n';
+  textArea.textContent += `*В ресторане:* ${inRes}\n`;
+  textArea.textContent += `*Кейтеринг:* ${keyT}\n`;
+  textArea.textContent += `*Агрегаторы:* ${agrE}\n`;
+  textArea.textContent += `*Алкоголь:* ${alko}\n`;
+
+  textArea.textContent += '\n';
+  /*
+  textArea.textContent += `*Дельта к БК 4 год (44):* ${new Intl.NumberFormat("ru", {style: "percent", minimumFractionDigits: 1}).format(bkS.value)}\n`;
+  textArea.textContent += `*SSS(21) Day:* ${new Intl.NumberFormat("ru", {style: "percent", minimumFractionDigits: 1}).format(dayS.value)}\n`;
+  textArea.textContent += `*SSS(21) Week:* ${new Intl.NumberFormat("ru", {style: "percent", minimumFractionDigits: 1}).format(weekS.value)}\n`;
+  textArea.textContent += `*SSS(21) Month:* ${new Intl.NumberFormat("ru", {style: "percent", minimumFractionDigits: 1}).format(monthS.value)}\n`;
+  */
+  textArea.textContent += `*Дельта к БК 4 год (44):* ${bkS.value} %\n`;
+  textArea.textContent += `*SSS(21) Day:* ${dayS.value} %\n`;
+  textArea.textContent += `*SSS(21) Week:* ${weekS.value} %\n`;
+  textArea.textContent += `*SSS(21) Month:* ${monthS.value} %\n`;
+
+  textArea.textContent += '\n';
+  textArea.textContent += `С уважением, ${manager}`
+}
+
+form.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  sms();
+})
