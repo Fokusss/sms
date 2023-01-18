b669f5bbf53d20553dc45bd40e7dccb804e73709
Arseniy Sazonov <rimus00@yandex.ru>
2023-01-19 02:00:27 +0300

    последние правки

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const bkMonth = 46;
-const bkYear = 4;
-const sssYear = 19;
+const bkMonth = localStorage.getItem('bkMonth');
+const bkYear = localStorage.getItem('bkYI');
+const sssYear = localStorage.getItem('yearSss');
 
 
 const form = document.querySelector('.form');
@@ -26,24 +26,41 @@ let monthS = document.querySelector('#monthS');
 let bkS = document.querySelector('#bkS');
 const manGS = document.querySelector('#manGS');
 const copy = document.querySelector('#copy');
+const popUp = document.querySelector('.pop-up');
+const buttonOpenPop = document.querySelector('.nast');
+const buttonClosePop = document.querySelector('.close');
+const buttonSave = document.querySelector('#saveOpt');
 
-function restoran () {
+function restoran() {
   const cheki = vse.value - platK.value - yaChe.value - delChe.value - kidChe.value - svoyaChe.value;
   const vsego = obj.value - yanV.value - delV.value - kidsV.value - svoyaV.value;
   const procent = vsego / obj.value;
-  const text = `${cheki} / ${new Intl.NumberFormat("ru", {style: "currency", currency: "RUB", minimumFractionDigits: 0}).format(vsego)} / ${new Intl.NumberFormat("ru", {style: "percent", minimumFractionDigits: 1}).format(procent)}`;
+  const text = `${cheki} / ${new Intl.NumberFormat("ru", { style: "currency", currency: "RUB", minimumFractionDigits: 0 }).format(vsego)} / ${new Intl.NumberFormat("ru", { style: "percent", minimumFractionDigits: 1 }).format(procent)}`;
   return text;
 }
 
 function prozrachnost() {
   const s = document.querySelector('#sss');
   const bk = document.querySelector('#bkM');
-  s.innerHTML+= ` (${sssYear})`;
-  bk.innerHTML+= ` (${bkMonth})`;
-
+  s.innerHTML = `SSS (${sssYear})`;
+  bk.innerHTML = `БК (${bkMonth})`;
+  manGS.add(new Option(localStorage.getItem('mang1'), 'mng1'));
+  manGS.add(new Option(localStorage.getItem('mang2'), 'mng2'));
+  manGS.add(new Option(localStorage.getItem('mang3'), 'mng3'));
+  document.querySelector('#mng1').value = localStorage.getItem('mang1');
+  document.querySelector('#mng2').value = localStorage.getItem('mang2');
+  document.querySelector('#mng3').value = localStorage.getItem('mang3');
+  document.querySelector('#bkkMonth').value = localStorage.getItem('bkMonth')
+  document.querySelector('#yuer').value = localStorage.getItem('yearSss')
+  document.querySelector('#bkYI').value = localStorage.getItem('bkYI')
+  document.querySelector('#trueKeyt').value = localStorage.getItem('trueKeyt')
+  if (localStorage.getItem('trueKeyt') === '0') {
+    sleepKeyt()
+  }
 }
 
-function keytering () {
+
+function keytering() {
   let text = '';
   if (kidChe.value == 0) {
     text += 0;
@@ -51,12 +68,12 @@ function keytering () {
     text += '\n';
     text += '*Кидбург:* ';
     const procent = kidsV.value / obj.value;
-    text += `${kidChe.value} / ${new Intl.NumberFormat("ru", {style: "currency", currency: "RUB", minimumFractionDigits: 0}).format(kidsV.value)} / ${new Intl.NumberFormat("ru", {style: "percent", minimumFractionDigits: 1}).format(procent)}`;
+    text += `${kidChe.value} / ${new Intl.NumberFormat("ru", { style: "currency", currency: "RUB", minimumFractionDigits: 0 }).format(kidsV.value)} / ${new Intl.NumberFormat("ru", { style: "percent", minimumFractionDigits: 1 }).format(procent)}`;
   }
   return text;
 }
 
-function agregators () {
+function agregators() {
   let text = '';
   let procent = 0;
   if (yaChe.value == 0 && delChe.value == 0 && svoyaChe.value == 0) {
@@ -66,57 +83,61 @@ function agregators () {
       text += '\n';
       text += '*Яндекс:*';
       procent = yanV.value / obj.value;
-      text += ` ${yaChe.value} / ${new Intl.NumberFormat("ru", {style: "currency", currency: "RUB", minimumFractionDigits: 0}).format(yanV.value)} / ${new Intl.NumberFormat("ru", {style: "percent", minimumFractionDigits: 1}).format(procent)}`;
+      text += ` ${yaChe.value} / ${new Intl.NumberFormat("ru", { style: "currency", currency: "RUB", minimumFractionDigits: 0 }).format(yanV.value)} / ${new Intl.NumberFormat("ru", { style: "percent", minimumFractionDigits: 1 }).format(procent)}`;
     }
 
     if (delChe.value > 0) {
       text += '\n';
       text += '*Delivery:*';
       procent = delV.value / obj.value;
-      text += ` ${delChe.value} / ${new Intl.NumberFormat("ru", {style: "currency", currency: "RUB", minimumFractionDigits: 0}).format(delV.value)} / ${new Intl.NumberFormat("ru", {style: "percent", minimumFractionDigits: 1}).format(procent)}`;
+      text += ` ${delChe.value} / ${new Intl.NumberFormat("ru", { style: "currency", currency: "RUB", minimumFractionDigits: 0 }).format(delV.value)} / ${new Intl.NumberFormat("ru", { style: "percent", minimumFractionDigits: 1 }).format(procent)}`;
     }
 
     if (svoyaChe.value > 0) {
       text += '\n';
       text += '*Своя:*';
       procent = svoyaV.value / obj.value;
-      text += ` ${svoyaChe.value} / ${new Intl.NumberFormat("ru", {style: "currency", currency: "RUB", minimumFractionDigits: 0}).format(svoyaV.value)} / ${new Intl.NumberFormat("ru", {style: "percent", minimumFractionDigits: 1}).format(procent)}`;
+      text += ` ${svoyaChe.value} / ${new Intl.NumberFormat("ru", { style: "currency", currency: "RUB", minimumFractionDigits: 0 }).format(svoyaV.value)} / ${new Intl.NumberFormat("ru", { style: "percent", minimumFractionDigits: 1 }).format(procent)}`;
     }
   }
   return text;
 }
 
-function alkogol () {
+function alkogol() {
   const procent = alcoG.value / obj.value;
-  const text = `${new Intl.NumberFormat("ru", {style: "currency", currency: "RUB", minimumFractionDigits: 0}).format(alcoG.value)} / ${new Intl.NumberFormat("ru", {style: "percent", minimumFractionDigits: 1}).format(procent)}`
+  const text = `${new Intl.NumberFormat("ru", { style: "currency", currency: "RUB", minimumFractionDigits: 0 }).format(alcoG.value)} / ${new Intl.NumberFormat("ru", { style: "percent", minimumFractionDigits: 1 }).format(procent)}`
   return text;
 }
 
-function manG () {
-  if (manGS.value === 'lena'){
-    return 'Стоянова Елена';
+function manG() {
+  if (manGS.value === 'mng1') {
+    return localStorage.getItem('mang1');
   }
-  if (manGS.value === 'vlad'){
-    return 'Бабкин Владислав';
+  if (manGS.value === 'mng2') {
+    return localStorage.getItem('mang2');
   }
-  if (manGS.value === 'arseniy'){
-    return 'Сазонов Арсений';
+  if (manGS.value === 'mng3') {
+    return localStorage.getItem('mang3');
   }
 }
 
-function sSs () {
+function sSs() {
   dayS.value = (dayS.value).replace(/,/g, '.');
   weekS.value = (weekS.value).replace(/,/g, '.');
   monthS.value = (monthS.value).replace(/,/g, '.');
   bkS.value = (bkS.value).replace(/,/g, '.');
 }
 
-function sms () {
+function KeytSmm(keyT) {
+  textArea.textContent += `*Кейтеринг:* ${keyT}\n`;
+}
+
+function sms() {
   sSs();
   const bkDat = new Date('September 11, 2018');
   const date = new Date();
   const inRes = restoran();
-  const keyT= keytering();
+  const keyT = keytering();
   const agrE = agregators();
   const alko = alkogol();
   const manager = manG();
@@ -125,24 +146,26 @@ function sms () {
   textArea.textContent += `*Отчёт по выручке ОМ ЦДМ за ${new Intl.DateTimeFormat().format(date)}*\n`;
   textArea.textContent += '\n';
 
-  textArea.textContent += `*Утро:* ${new Intl.NumberFormat("ru", {style: "currency", currency: "RUB", minimumFractionDigits: 0}).format(utro.value)}\n`;
-  textArea.textContent += `*День:* ${new Intl.NumberFormat("ru", {style: "currency", currency: "RUB", minimumFractionDigits: 0}).format(den.value)}\n`;
-  textArea.textContent += `*Вечер:* ${new Intl.NumberFormat("ru", {style: "currency", currency: "RUB", minimumFractionDigits: 0}).format(vech.value)}\n`;
-  textArea.textContent += `*Общая:* ${new Intl.NumberFormat("ru", {style: "currency", currency: "RUB", minimumFractionDigits: 0}).format(obj.value)}\n`;
+  textArea.textContent += `*Утро:* ${new Intl.NumberFormat("ru", { style: "currency", currency: "RUB", minimumFractionDigits: 0 }).format(utro.value)}\n`;
+  textArea.textContent += `*День:* ${new Intl.NumberFormat("ru", { style: "currency", currency: "RUB", minimumFractionDigits: 0 }).format(den.value)}\n`;
+  textArea.textContent += `*Вечер:* ${new Intl.NumberFormat("ru", { style: "currency", currency: "RUB", minimumFractionDigits: 0 }).format(vech.value)}\n`;
+  textArea.textContent += `*Общая:* ${new Intl.NumberFormat("ru", { style: "currency", currency: "RUB", minimumFractionDigits: 0 }).format(obj.value)}\n`;
   textArea.textContent += '\n';
 
   textArea.textContent += '*Из них:*\n';
   textArea.textContent += `*В ресторане:* ${inRes}\n`;
-  textArea.textContent += `*Кейтеринг:* ${keyT}\n`;
+  if (localStorage.getItem('trueKeyt') != '0') {
+    KeytSmm(keyT)
+  }
   textArea.textContent += `*Агрегаторы:* ${agrE}\n`;
   textArea.textContent += `*Алкоголь:* ${alko}\n`;
 
   textArea.textContent += '\n';
 
-  textArea.textContent += `*Дельта к БК ${bkYear} год (${bkMonth}):* ${new Intl.NumberFormat("ru", {style: "percent", minimumFractionDigits: 1}).format(bkS.value/100)}\n`;
-  textArea.textContent += `*SSS(${sssYear}) Day:* ${new Intl.NumberFormat("ru", {style: "percent", minimumFractionDigits: 1}).format(dayS.value/100)}\n`;
-  textArea.textContent += `*SSS(${sssYear}) Week:* ${new Intl.NumberFormat("ru", {style: "percent", minimumFractionDigits: 1}).format(weekS.value/100)}\n`;
-  textArea.textContent += `*SSS(${sssYear}) Month:* ${new Intl.NumberFormat("ru", {style: "percent", minimumFractionDigits: 1}).format(monthS.value/100)}\n`;
+  textArea.textContent += `*Дельта к БК ${bkYear} год (${bkMonth}):* ${new Intl.NumberFormat("ru", { style: "percent", minimumFractionDigits: 1 }).format(bkS.value / 100)}\n`;
+  textArea.textContent += `*SSS(${sssYear}) Day:* ${new Intl.NumberFormat("ru", { style: "percent", minimumFractionDigits: 1 }).format(dayS.value / 100)}\n`;
+  textArea.textContent += `*SSS(${sssYear}) Week:* ${new Intl.NumberFormat("ru", { style: "percent", minimumFractionDigits: 1 }).format(weekS.value / 100)}\n`;
+  textArea.textContent += `*SSS(${sssYear}) Month:* ${new Intl.NumberFormat("ru", { style: "percent", minimumFractionDigits: 1 }).format(monthS.value / 100)}\n`;
 
   textArea.textContent += '\n';
   textArea.textContent += `С уважением, ${manager}`
@@ -159,7 +182,37 @@ copy.addEventListener('click', () => {
   document.execCommand('copy');
 })
 
+function openPopup() {
+  popUp.classList.add('pop-up_active');
+}
 
+function hidePopUp() {
+  popUp.classList.remove('pop-up_active');
+}
+
+function saveLocal(e) {
+  localStorage.setItem('mang1', document.querySelector('#mng1').value)
+  localStorage.setItem('mang2', document.querySelector('#mng2').value)
+  localStorage.setItem('mang3', document.querySelector('#mng3').value)
+  localStorage.setItem('bkMonth', document.querySelector('#bkkMonth').value);
+  localStorage.setItem('yearSss', document.querySelector('#yuer').value);
+  localStorage.setItem('bkYI', document.querySelector('#bkYI').value)
+  localStorage.setItem('trueKeyt', document.querySelector('#trueKeyt').value)
+  hidePopUp();
+}
+
+
+function sleepKeyt() {
+  document.querySelector('#aa').classList.add('none')
+  document.querySelector('#bb').classList.add('none')
+  document.querySelector('#kidChe').classList.add('none')
+  document.querySelector('#kidsV').classList.add('none')
+}
+
+
+buttonOpenPop.addEventListener('click', openPopup);
+buttonClosePop.addEventListener('click', hidePopUp);
+buttonSave.addEventListener('click', saveLocal);
 
 
 prozrachnost();
